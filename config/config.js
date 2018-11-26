c9540abf3896805ff71f31ac598479225e050a62
Shabanov GM <gorgshabanov@gmail.com>
2018-11-26 21:37:40 +0300

    version 1.0.0

---
+++ config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    TITLE: 'h1.section-hero-header-title',
+    SUB_TITLE: 'h2.section-hero-header-subtitle',
+    FEEDBACK_COUNT: 'button.widget-pane-link',
+    BUTTON_FEEDBACK: '.widget-pane-link', 
+    FEEDBACK_LIST: 'section-listbox',
+    CURRENT_FEEDBACKS: 'section-review',
+    STILL_FEDDBACK_CONTENT: '.section-expand-review',
+    FEEDBACK_ACCOUNT_HREF: 'div > a',
+    FEEDBACK_ACCOUNT_NAME: 'div.section-review-title > span',
+    FEEDBACK_STARS: 'span.section-review-stars',
+    FEEDBACK_DATE: 'span.section-review-publish-date',
+    FEEDBACK_CONTENT: 'div.section-review-review-content > span.section-review-text',
+    FEEDBACK_OWNER_RESPONSE_DATE: 'span.section-review-owner-response-subtitle',
+    FEEDBACK_OWNER_RESPONSE_CONTENT: 'div.section-review-text',
+}
